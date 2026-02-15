62220d3fa60b110b9da038912b2c5460798f721c
LENOVO <LENOVO@DESKTOP-KRRHIQS>
2026-02-15 15:37:38 +0300

    95. ders sonu

---
+++ ApiProjeKampi.WebUI/Dtos/AISuggestionDtos/AIDailyMenuSuggestionItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebUI.Dtos.AISuggestionDtos
+{
+    public class AIDailyMenuSuggestionItem
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Price { get; set; }
+        public string Reason { get; set; }
+    }
+}
